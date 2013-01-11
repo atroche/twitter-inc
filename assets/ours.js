2291f335fa5d13ea6615fe4fbfea9d1c7553c06f
Alistair Roche <roche.a@gmail.com>
2013-01-11 16:58:26 +1100

    Submitted email via modal

--- assets/ours.js
+++ assets/ours.js
@@ -24,4 +24,22 @@ $(function(){
             $counter.text(currentVal);
         }
     }, 100);
-})
+
+    $("a.get-started").leanModal({closeButton: ".modal_close"});
+    $('form#email').submit(function(){
+        var emailInput = $(this).find('input[name=email]');
+        var newEmail = emailInput.val();
+        var key = 'TriviaIncEmails';
+        remoteStorage.getItem(key, function(emails){
+            var emails = emails.split(',');
+            emails.push(newEmail);
+            remoteStorage.setItem(key, emails);
+        });
+
+        emailInput.val('');
+        return false;
+    });
+
+});
+
+
